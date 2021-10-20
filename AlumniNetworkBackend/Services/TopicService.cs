4b3c0add08d13960e16a882ed6e3b9c4dc50d438
Sergei Kaukiainen <sergei.kaukiainen@5CG04676KY>
2021-10-20 22:44:12 +0300

    Add service for topics

---
+++ AlumniNetworkBackend/Services/TopicService.cs
@@ -0,0 +1,40 @@
+﻿using AlumniNetworkBackend.Models;
+using AlumniNetworkBackend.Models.Domain;
+using AlumniNetworkBackend.Models.DTO.TopicDTO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlumniNetworkBackend.Services
+{
+    public class TopicService: ITopicService
+    {
+        private readonly AlumniNetworkDbContext _context;
+
+        public TopicService(AlumniNetworkDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Topic> Create(Topic newTopic, User user)
+        {
+            var returnValue = _context.Topics.Add(newTopic);
+            await _context.SaveChangesAsync();
+            await UpdateUsers(returnValue.Entity, user);
+            return returnValue.Entity;
+        }
+
+        public async Task<Topic> UpdateUsers(Topic topic, User user)
+        {
+            
+            Topic topicToUpdateUsers = await _context.Topics.Include(u => u.Users).Where(t => t.Id == topic.Id).FirstAsync();
+            List<User> userList = new() { user };
+            topicToUpdateUsers.Users = userList;
+            await _context.SaveChangesAsync();
+
+            return topicToUpdateUsers;
+        }
+    }
+}
