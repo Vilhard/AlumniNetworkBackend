0a467b45370483d9aa89bad5954e4a1908024817
Omar El Tokhy <omar.el.tokhy@fi.experis.com>
2021-10-25 16:01:43 +0300

    Edit. edited mock data

--- AlumniNetworkBackend/Models/AlumniNetworkDbContext.cs
+++ AlumniNetworkBackend/Models/AlumniNetworkDbContext.cs
@@ -25,7 +25,7 @@ namespace AlumniNetworkBackend.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=5CG05206QT\\SQLEXPRESS;Initial Catalog=AlumniNetworkDB;Integrated Security=True;");
+            optionsBuilder.UseSqlServer("Data Source=5CG05206QY\\SQLEXPRESS;Initial Catalog=AlumniNetworkDB;Integrated Security=True;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -79,7 +79,7 @@ namespace AlumniNetworkBackend.Models
                 Name = "Noroff Graduation",
                 Description = "Noroff Graduation party",
                 AllowGuests = true,
-                BannerImg = "noroff.jpg",
+                BannerImg = "https://keystoneacademic-res.cloudinary.com/image/upload/element/12/121913_121910_Noroff-logo.png",
                 StartTime = new DateTime(2021, 12, 12, 19, 30, 0),
                 EndTime = new DateTime(2021, 12, 12, 23, 30, 0),
                 CreatedById = "2",
@@ -95,7 +95,7 @@ namespace AlumniNetworkBackend.Models
                 Name = "Experis Graduation",
                 Description = "Experis Graduation party",
                 AllowGuests = true,
-                BannerImg = "experis.jpg",
+                BannerImg = "http://experisacademy.no/storage/images/fbook.jpg",
                 StartTime = new DateTime(2021, 12, 12, 19, 30, 0),
                 EndTime = new DateTime(2021, 12, 12, 23, 30, 0),
                 CreatedById = "2",
@@ -111,7 +111,7 @@ namespace AlumniNetworkBackend.Models
                 Name = "Just A Party",
                 Description = "Just A Party for people who want to get wasted for no reason",
                 AllowGuests = true,
-                BannerImg = "party.jpg",
+                BannerImg = "https://cdn.pixabay.com/photo/2019/11/14/03/22/party-4625237_960_720.png",
                 StartTime = new DateTime(2021, 12, 12, 19, 30, 0),
                 EndTime = new DateTime(2021, 12, 12, 23, 30, 0),
                 CreatedById = "1",
@@ -127,7 +127,7 @@ namespace AlumniNetworkBackend.Models
                 Name = "Me, myself and I",
                 Description = "Event just for me",
                 AllowGuests = false,
-                BannerImg = "allbymyself.jpg",
+                BannerImg = "https://ak.picdn.net/shutterstock/videos/1009460807/thumb/1.jpg",
                 StartTime = new DateTime(2021, 12, 12, 19, 30, 0),
                 EndTime = new DateTime(2021, 12, 12, 23, 30, 0),
                 CreatedById = "4",
@@ -183,7 +183,7 @@ namespace AlumniNetworkBackend.Models
                 Text = "What's up everybody?",
                 TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
                 SenderId = "1",
-               // ReplyParentId = null,
+                ReplyParentId = null,
                 TargetEventId = null,
                 TargetGroupId = null,
                 TargetTopicId = null,
@@ -195,7 +195,7 @@ namespace AlumniNetworkBackend.Models
                 Text = "Feeling good!",
                 TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
                 SenderId = "2",
-               // ReplyParentId = 1,
+                ReplyParentId = 1,
                 TargetEventId = null,
                 TargetGroupId = null,
                 TargetTopicId = null,
@@ -207,7 +207,7 @@ namespace AlumniNetworkBackend.Models
                 Text = "Today's going to be a good day!",
                 TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
                 SenderId = "3",
-               // ReplyParentId = 1,
+                ReplyParentId = 1,
                 TargetEventId = null,
                 TargetGroupId = null,
                 TargetTopicId = null,
@@ -219,7 +219,343 @@ namespace AlumniNetworkBackend.Models
                 Text = "Is there any good Events to attend anytime soon?",
                 TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
                 SenderId = "4",
-               // ReplyParentId = null,
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 5,
+                Text = "How's everybody doing today?",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "1",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 6,
+                Text = "Anybody know a good place to get ramen?",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "2",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 7,
+                Text = "I heard Momotoko is pretty good",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "3",
+                ReplyParentId = 6,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 8,
+                Text = "Whats happening this friday?",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "4",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 9,
+                Text = "I heard there is a graduation party at Experis!",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "1",
+                ReplyParentId = 8,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 10,
+                Text = "Harry Potter stuff is cool!",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "2",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = 1,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 11,
+                Text = "Yeah it's awesome!",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "3",
+                ReplyParentId = 1,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = 1,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 12,
+                Text = "Just saw these movies and they were good!",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "4",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = 2,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 13,
+                Text = "I agree!",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "1",
+                ReplyParentId = 12,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = 2,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 14,
+                Text = "The original trilogy was the best!",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "2",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = 3,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 15,
+                Text = "Thor is awesome!",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "3",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = 4,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 16,
+                Text = "Coding is fun!",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "4",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = 1,
+                TargetTopicId = null,
+                TargetUserId = null
+            }); 
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 17,
+                Text = "I liked React more than Angular",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "1",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = 1,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 18,
+                Text = "Yeah me too",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "2",
+                ReplyParentId = 17,
+                TargetEventId = null,
+                TargetGroupId = 1,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 19,
+                Text = "Experis is a nice place to work!",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "3",
+                ReplyParentId = 1,
+                TargetEventId = null,
+                TargetGroupId = 2,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 20,
+                Text = "I love Marvel comics",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "4",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = 3,
+                TargetTopicId = null,
+                TargetUserId = "2"
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 21,
+                Text = "Me too",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "1",
+                ReplyParentId = 20,
+                TargetEventId = null,
+                TargetGroupId = 3,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 22,
+                Text = "This party is going to be awesome!",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "2",
+                ReplyParentId = null,
+                TargetEventId = 1,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 23,
+                Text = "Are we like full stack developers now?",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "3",
+                ReplyParentId = null,
+                TargetEventId = 1,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 24,
+                Text = "Soo, are we getting like, paid from now on?",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "4",
+                ReplyParentId = null,
+                TargetEventId = 2,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = "2"
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 25,
+                Text = "Should we play beer pong or something?",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "4",
+                ReplyParentId = null,
+                TargetEventId = 3,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 26,
+                Text = "That sounds good!",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "2",
+                ReplyParentId = 25,
+                TargetEventId = 3,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 27,
+                Text = "In Finland we call this kalsarikännit",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "4",
+                ReplyParentId = null,
+                TargetEventId = 4,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = null
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 28,
+                Text = "Hey",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "3",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = "2"
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 29,
+                Text = "Hey",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "2",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = "3"
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 30,
+                Text = "How are yu?",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "3",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = "2"
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 31,
+                Text = "I'm fine, how about you?",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "2",
+                ReplyParentId = null,
+                TargetEventId = null,
+                TargetGroupId = null,
+                TargetTopicId = null,
+                TargetUserId = "3"
+            });
+            modelBuilder.Entity<Post>().HasData(new Post()
+            {
+                Id = 32,
+                Text = "I'm also fine thanks :)",
+                TimeStamp = new DateTime(2021, 12, 12, 19, 30, 0),
+                SenderId = "3",
+                ReplyParentId = null,
                 TargetEventId = null,
                 TargetGroupId = null,
                 TargetTopicId = null,
@@ -232,9 +568,10 @@ namespace AlumniNetworkBackend.Models
                 Id = "1",
                 Name = "Harry Potter",
                 Username = "TheBoyWhoLives",
-                Bio = "",
-                Status = "",
-                FunFact = "",
+                Bio = "Magical",
+                Status = "Feeling good",
+                FunFact = "Voldemort tries to kill me",
+                Picture = "https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg",
                 Events = {},
                 Topics = {},
                 Groups = {} 
@@ -244,9 +581,10 @@ namespace AlumniNetworkBackend.Models
                 Id = "2",
                 Name = "Hermione Granger",
                 Username = "TheGirlWhoReads",
-                Bio = "",
-                Status = "",
-                FunFact = "",
+                Bio = "I like reading",
+                Status = "Reading books",
+                FunFact = "I secretly like Ron",
+                Picture = "https://data.topquizz.com/distant/quizz/big/6/0/9/9/219906_527364a914.jpg",
                 Events = { },
                 Topics = { },
                 Groups = { }
@@ -256,9 +594,10 @@ namespace AlumniNetworkBackend.Models
                 Id = "3",
                 Name = "Ron Weasley",
                 Username = "TheBoyWhoGinger",
-                Bio = "",
-                Status = "",
-                FunFact = "",
+                Bio = "Huh?",
+                Status = "Eating candy",
+                FunFact = "I secretly like Hermione",
+                Picture = "https://data.topquizz.com/distant/quizz/big/4/5/4/9/239454_33e86ba948.jpg",
                 Events = { },
                 Topics = { },
                 Groups = { }
@@ -268,9 +607,10 @@ namespace AlumniNetworkBackend.Models
                 Id = "4",
                 Name = "Keijo Johnson",
                 Username = "TheMan",
-                Bio = "",
-                Status = "",
-                FunFact = "",
+                Bio = "I am da boss",
+                Status = "Just bossing around",
+                FunFact = "I'm actually pretty lame",
+                Picture = "https://yt3.ggpht.com/ytc/AKedOLRkGd7s8oce9O7c7YCx4jjhszbfeM5UKJU0deeENA=s900-c-k-c0x00ffffff-no-rj",
                 Events = { },
                 Topics = { },
                 Groups = { }
