86bc64c5b8dfa3f5ad1ae9dfb2570a2927bf766a
ville <ville.hotakainen@gmail.com>
2021-11-01 23:24:08 +0200

    Feat: azure database authentication configuration added

--- AlumniNetworkBackend/Program.cs
+++ AlumniNetworkBackend/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
 
 namespace AlumniNetworkBackend
 {
@@ -18,9 +22,27 @@ namespace AlumniNetworkBackend
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, builder) =>
+            {
+                var keyVaultEndpoint = GetKeyVaultEndpoint();
+                if (!string.IsNullOrEmpty(keyVaultEndpoint))
+                {
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var keyVaultClient = new KeyVaultClient(
+                        new KeyVaultClient.AuthenticationCallback(
+                            azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                    builder.AddAzureKeyVault(
+                        keyVaultEndpoint,
+                        keyVaultClient,
+                        new DefaultKeyVaultSecretManager());
+                }
+            })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                   
                 });
+        private static string GetKeyVaultEndpoint() => "https://alumnikeyvault001.vault.azure.net/";
     }
 }
